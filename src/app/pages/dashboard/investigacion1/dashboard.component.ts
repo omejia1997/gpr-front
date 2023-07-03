5294d81b04d3a4ae72f882bf7d00bc4cb34cc03a
omejia1997 <omardevaluado08@gmail.com>
2023-07-03 10:56:42 -0500

    subiendo cambios para filtrar datos

--- src/app/pages/dashboard/investigacion1/dashboard.component.ts
+++ src/app/pages/dashboard/investigacion1/dashboard.component.ts
@@ -1,43 +1,52 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { Observable } from "rxjs";
-import { DashboardProyectoInvestigacion } from "src/app/models/Dashboard/DashboardProyectoInvestigacion";
-import { TareaService } from "src/app/servicios/tarea.service";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DashboardProyectoInvestigacion } from 'src/app/models/Dashboard/DashboardProyectoInvestigacion';
+import { TareaService } from 'src/app/servicios/tarea.service';
 @Component({
-  selector: "app-dashboard",
-  templateUrl: "dashboard-investigacion.component.html",
-  styleUrls: ['./dashboard-investigacion.component.css']
+  selector: 'app-dashboard',
+  templateUrl: 'dashboard-investigacion.component.html',
+  styleUrls: ['./dashboard-investigacion.component.css'],
 })
 export class DashboardInvestigacionComponent implements OnInit {
+  searchTerm!: string;
+  dataFiltrada!: any[];
   //view: any[2] = [500, 400];
-  dataProyecto: DashboardProyectoInvestigacion[]=[];
-  getDashboardProyectoInvestigacion$: Observable<DashboardProyectoInvestigacion[]>;
+  dataProyecto: any[] = [];
+  getDashboardProyectoInvestigacion$: Observable<
+    DashboardProyectoInvestigacion[]
+  >;
 
-  constructor(
-    private tareaService: TareaService,
-    private router: Router
-  ) {
-    this.getDashboardProyectoInvestigacion$ = this.tareaService.obtenerDatosProyectoDashboardInvestigacion(2);
+  constructor(private tareaService: TareaService, private router: Router) {
+    this.getDashboardProyectoInvestigacion$ =
+      this.tareaService.obtenerDatosProyectoDashboardInvestigacion(2);
   }
 
   ngOnInit() {
     this.getData();
   }
 
-  getData(){
-    this.getDashboardProyectoInvestigacion$.subscribe(d => {
-     this.dataProyecto = d;
-     console.log(d)
-    })
+  search() {
+    if (this.searchTerm)
+      this.dataFiltrada = this.dataProyecto.filter((obj) =>
+        obj.name.includes(this.searchTerm.toUpperCase())
+      );
+    else this.dataFiltrada = this.dataProyecto;
   }
 
-  onSelect(event:any) {
+  getData() {
+    this.getDashboardProyectoInvestigacion$.subscribe((d) => {
+      this.dataProyecto = d;
+      this.search();
+    });
+  }
+
+  onSelect(event: any) {
     console.log(event);
   }
 
-  verEstadoProyecto(proyecto:DashboardProyectoInvestigacion){
+  verEstadoProyecto(proyecto: DashboardProyectoInvestigacion) {
     this.tareaService.setDashboardProyectoInvestigacion(proyecto);
     this.router.navigate(['dashboard-tarea-investigacion']);
   }
 }
-
