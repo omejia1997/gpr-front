cde8eb859652160af827ed69598bd01dd2a769c4
omejia1997 <omardevaluado08@gmail.com>
2023-07-09 21:06:10 -0500

    subiendo cambios de url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   URL_STORAGE: "https://documents-grp-decem.azurewebsites.net",//http://localhost:8090
   URL_MICROSERVICE_VINCULACION: "http://localhost:8088",
-  URL_MICROSERVICE_INVESTIGACION: "http://localhost:8080"
+  URL_MICROSERVICE_INVESTIGACION: "https://gpr-decem-espe.azurewebsites.net"//http://localhost:8080
 };
 
 /*
