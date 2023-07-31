e2bb9a5884e6f75cf213421927c43e90f9f59bd4
omejia1997 <omardevaluado08@gmail.com>
2023-07-31 12:34:42 -0500

    subiendo a https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   URL_STORAGE: "https://documents-grp-decem.azurewebsites.net",
-  URL_MICROSERVICE_VINCULACION:"http://gpr-espe-vinculacion.azurewebsites.net",
+  URL_MICROSERVICE_VINCULACION:"https://gpr-espe-vinculacion.azurewebsites.net",
   URL_MICROSERVICE_INVESTIGACION: "https://gpr-decem-espe.azurewebsites.net",
   URL_MICROSERVICE_DOCENTE_INFORMACION: "http://localhost:8888",
   URL_MICROSERVICE_DOCENTE_TAREAS: "http://localhost:9999"
